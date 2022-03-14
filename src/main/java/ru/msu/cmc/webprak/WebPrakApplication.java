adc531c9bd6ba88a64bf8558c9e1df202837dfd7
Daniil Subbotin <danya.subbotin34@gmail.com>
2022-03-14 20:03:34 +0300

    spring init

---
+++ src/main/java/ru/msu/cmc/webprak/WebPrakApplication.java
@@ -0,0 +1,13 @@
+package ru.msu.cmc.webprak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPrakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPrakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/msu/cmc/webprak/WebPrakApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.msu.cmc.webprak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPrakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
