87d489d6367a0178a3e13f8d5802ce861f490b5c
Daniil Subbotin <danya.subbotin34@gmail.com>
2022-03-25 02:56:06 +0300

    new config

--- src/main/java/ru/msu/cmc/webprak/HibernateDatabaseConfig.java
+++ src/main/java/ru/msu/cmc/webprak/HibernateDatabaseConfig.java
@@ -1,4 +1,43 @@
 package ru.msu.cmc.webprak;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
 public class HibernateDatabaseConfig {
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(oraDataSource());
+        sessionFactory.setPackagesToScan("ru.msu.cmc.webprak.models");
+
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL10Dialect");
+        hibernateProperties.setProperty("connection_pool_size", "1");
+
+        sessionFactory.setHibernateProperties(hibernateProperties);
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource oraDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost/Website");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("");
+
+        return dataSource;
+    }
 }
