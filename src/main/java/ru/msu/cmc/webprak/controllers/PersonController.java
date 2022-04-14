64f23ba34315a0dd9a672dfdc20155bb230e9d2a
Daniil Subbotin <danya.subbotin34@gmail.com>
2022-04-15 00:27:58 +0300

    upgrade persons page

--- src/main/java/ru/msu/cmc/webprak/controllers/PersonController.java
+++ src/main/java/ru/msu/cmc/webprak/controllers/PersonController.java
@@ -6,26 +6,41 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import ru.msu.cmc.webprak.DAO.Person2PlaceDAO;
+import ru.msu.cmc.webprak.DAO.PersonDAO;
+import ru.msu.cmc.webprak.DAO.PlaceDAO;
+import ru.msu.cmc.webprak.DAO.RelationDAO;
+import ru.msu.cmc.webprak.DAO.impl.Person2PlaceDAOImpl;
 import ru.msu.cmc.webprak.DAO.impl.PersonDAOImpl;
 import ru.msu.cmc.webprak.DAO.impl.PlaceDAOImpl;
+import ru.msu.cmc.webprak.DAO.impl.RelationDAOImpl;
 import ru.msu.cmc.webprak.models.Person;
+import ru.msu.cmc.webprak.models.Relation;
 
 import java.sql.Date;
 import java.util.List;
 
 @Controller
 public class PersonController {
-    @Autowired
-    private final PersonDAOImpl personDAO = new PersonDAOImpl();
 
     @Autowired
-    private final PlaceDAOImpl placeDAO = new PlaceDAOImpl();
+    private final PersonDAO personDAO = new PersonDAOImpl();
+
+    @Autowired
+    private final PlaceDAO placeDAO = new PlaceDAOImpl();
+
+    @Autowired
+    private final Person2PlaceDAO person2PlaceDAO = new Person2PlaceDAOImpl();
+
+    @Autowired
+    private final RelationDAO relationDAO = new RelationDAOImpl();
 
     @GetMapping("/persons")
     public String peopleListPage(Model model) {
         List<Person> people = (List<Person>) personDAO.getAll();
         model.addAttribute("people", people);
         model.addAttribute("personService", personDAO);
+        model.addAttribute("relationService", relationDAO);
         return "persons";
     }
 
@@ -41,6 +56,8 @@ public class PersonController {
         model.addAttribute("person", person);
         model.addAttribute("personService", personDAO);
         model.addAttribute("placeService", placeDAO);
+        model.addAttribute("person2placeService", person2PlaceDAO);
+        model.addAttribute("relationService", relationDAO);
         return "person";
     }
 
