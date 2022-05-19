947b7c1efe2236d9303f1afc398a8d77ce34a0e5
Daniil Subbotin <danya.subbotin34@gmail.com>
2022-05-19 15:25:34 +0300

    some tests added

--- src/test/java/ru/msu/cmc/webprak/WebTest.java
+++ src/test/java/ru/msu/cmc/webprak/WebTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,13 +13,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class WebTest {
 
     private final String rootTitle = "Главная страница";
-
     private final String peopleTitle = "Люди";
     private final String placesTitle = "Места";
-
     private final String placeTitle = "Информация о месте";
-
-    private final String editPlaceTitle = "Редактировать место";
 
     @Test
     void MainPage() {
@@ -66,6 +63,7 @@ public class WebTest {
         WebElement addPlace = driver.findElement(By.id("addPlaceButton"));
         addPlace.click();
         driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
+        String editPlaceTitle = "Редактировать место";
         assertEquals(editPlaceTitle, driver.getTitle());
 
         driver.findElement(By.id("placeName")).sendKeys("Тестовое место");
@@ -80,6 +78,97 @@ public class WebTest {
         assertEquals(cells.get(0).getText(), "Название: Тестовое место");
         assertEquals(cells.get(1).getText(), "Описание: Тестовое описание");
 
+        driver.quit();
+    }
+
+    @Test
+    void getPlaceInfo() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http://localhost:8080/place?placeId=1");
+        assertEquals(placeTitle, driver.getTitle());
+
+        WebElement placeInfo = driver.findElement(By.id("placeInfo"));
+        List<WebElement> cells = placeInfo.findElements(By.tagName("p"));
+
+        assertEquals(cells.get(0).getText(), "Название: Королевство Темерия");
+        assertEquals(cells.get(1).getText(), "Описание: Одно из самых крупных и могущественных королевств Севера, имеющее важное как стратегическое, так и политическое значение");
+
+        List<WebElement> personsFromPlace = cells.get(2).findElements(By.tagName("a"));
+        assertEquals(personsFromPlace.size(), 8);
+        assertEquals(personsFromPlace.get(0).findElement(By.tagName("span")).getText(), "Фольтест Темерский,");
+        assertEquals(personsFromPlace.get(1).findElement(By.tagName("span")).getText(), "Адда Белая,");
+        assertEquals(personsFromPlace.get(2).findElement(By.tagName("span")).getText(), "Цирилла Фиона Элен Рианнон,");
+        assertEquals(personsFromPlace.get(3).findElement(By.tagName("span")).getText(), "Эмгыр вар Эмрейс,");
+
+        driver.quit();
+    }
+
+    @Test
+    void getPersonInfo() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http://localhost:8080/person?personId=8");
+        String personTitle = "Информация о человеке";
+        assertEquals(personTitle, driver.getTitle());
+
+        WebElement personInfo = driver.findElement(By.id("personInfo"));
+        List<WebElement> cells = personInfo.findElements(By.tagName("p"));
+
+        assertEquals(cells.get(0).getText(), "Имя: Эмгыр вар Эмрейс");
+        assertEquals(cells.get(1).getText(), "Пол: Муж");
+        assertEquals(cells.get(2).getText(), "Дата рождения: 1220");
+        assertEquals(cells.get(3).getText(), "Дата смерти: 1290");
+        assertEquals(cells.get(4).getText(), "Законнорожденный: да");
+        assertEquals(cells.get(5).getText(), "Краткая характеристика: Император Нильфгаарда. Белое Пламя, Пляшущее на Курганах Врагов.");
+
+        List<WebElement> placeForPerson = cells.get(6).findElements(By.tagName("a"));
+        assertEquals(placeForPerson.size(), 3);
+        assertEquals(placeForPerson.get(0).findElement(By.tagName("span")).getText(), "Королевство Темерия,");
+        assertEquals(placeForPerson.get(1).findElement(By.tagName("span")).getText(), "Империя Нильфгаард,");
+        assertEquals(placeForPerson.get(2).findElement(By.tagName("span")).getText(), "Цинтра");
+
+        driver.quit();
+    }
+
+    @Test
+    void getParents() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http://localhost:8080/persons");
+        assertEquals(peopleTitle, driver.getTitle());
+
+        WebElement table = driver.findElement(By.tagName("table"));
+        List<WebElement> cells = table.findElements(By.tagName("tr"));
+        WebElement Cirilla = null;
+        for (WebElement elem : cells) {
+            List<WebElement> info = elem.findElements(By.tagName("td"));
+            if (info.size() == 0) {
+                continue;
+            }
+            if (Objects.equals(info.get(0).findElement(By.tagName("span")).getText(), "Цирилла Фиона Элен Рианнон")) {
+                Cirilla = elem;
+                break;
+            }
+        }
+        assert Cirilla != null;
+        List<WebElement> ciriInfo = Cirilla.findElements(By.tagName("td"));
+        assertEquals(ciriInfo.get(1).findElement(By.tagName("span")).getText(), "1252-неизвестно");
+        assertEquals(ciriInfo.get(2).findElement(By.tagName("span")).getText(), "Паветта Фиона Элен");
+        assertEquals(ciriInfo.get(3).findElement(By.tagName("span")).getText(), "Эмгыр вар Эмрейс");
+
+        ciriInfo.get(2).findElement(By.tagName("a")).click();
+        driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
+
+        assertEquals("Информация о человеке", driver.getTitle());
+
+
+        WebElement personInfo = driver.findElement(By.id("personInfo"));
+        List<WebElement> personCells = personInfo.findElements(By.tagName("p"));
+
+        assertEquals(personCells.get(0).getText(), "Имя: Паветта Фиона Элен");
+        assertEquals(personCells.get(1).getText(), "Пол: Жен");
+        assertEquals(personCells.get(2).getText(), "Дата рождения: 1236");
+        assertEquals(personCells.get(3).getText(), "Дата смерти: 1256");
+        assertEquals(personCells.get(4).getText(), "Законнорожденный: да");
+        assertEquals(personCells.get(5).getText(), "Краткая характеристика: Принцесса Цинтры. Носитель гена Старшей крови.");
         driver.quit();
     }
 }
